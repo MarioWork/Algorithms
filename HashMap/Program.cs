098396dbbf0958584f2b6e8a91d6910518c6fe59
Mario Vieria <47696178+MarioWork@users.noreply.github.com>
2020-12-20 02:22:14 -0100

    Add files via upload

---
+++ HashMap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashMap
+{
+    class Program
+    {        
+        static void FindPair(int[]array, int sum){
+            Dictionary <int,int> diction = new Dictionary<int, int>();
+            
+            for(int i = 0; i < array.Length; i++){
+
+                if(diction.ContainsKey(sum - array[i])){                    
+                    Console.WriteLine("Pair found at: "+diction[sum - array[i]] + 
+                        " and "+i);
+                        return;
+                }
+
+                diction.Add(array[i],i);                
+            }
+ 
+            Console.WriteLine("No pairs found.");
+        }
+        static void Main(string[] args)
+        {
+            int[]ar = { 4, 1, 2, 6, 3, 9 };
+            int sum = 5;
+            FindPair(ar,sum);
+        }
+
+    }
+}
